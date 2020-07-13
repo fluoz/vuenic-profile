7aff0bb6f451f8e5b663d347169c64c39fc9a8e7
fluoz <hasanabdulah31959@gmail.com>
2020-07-13 15:18:22 +0700

    Flex-Layout Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TranslationWidth } from '@angular/common';
+import { Subscription, from } from 'rxjs';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'vuenic-profile';
+  mediaSub: Subscription;
+  constructor(public mediaObserver: MediaObserver) { };
+  ngOnInit() {
+    this.mediaSub = this.mediaObserver.media$.subscribe((result: MediaChange) => {
+      console.log(result.mqAlias);
+    })
+  };
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
+  };
+
 }
+
+const jumbotron = document.querySelector('.jumbotron');
+const jumbo = document.querySelector('.jumbo');
+const thumb = document.querySelector('.thumb');
+const thumb2 = document.querySelector('.thumb2');
+
